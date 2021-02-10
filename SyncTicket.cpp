617830b9c529d629599b0c2aedcd56ba1d067963
berkekavak <madness1358>
2021-02-10 22:57:55 +0300

    Reservations are correct, messages are not ordered

--- SyncTicket.cpp
+++ SyncTicket.cpp
@@ -16,36 +16,43 @@ struct clientInfo {
     int arrivalTime, serviceTime, seatNumber;
 };
 
-#define BUFFER_SIZE 100
+#define BUFFER_SIZE 3
 #define MAX_ITEMS 10
 
 typedef struct clientInfo* buffer_item;
 int START_NUMBER = 0;
 buffer_item buffer[BUFFER_SIZE];
 
-pthread_mutex_t mutex;
+pthread_mutex_t mutex, reservationMutex;
 sem_t empty;
 sem_t full;
 
-int insertPointer = 0, removePointer = 0;
+int insertPointer = 0, removePointer = 0, theatreCapacity;
 bool *reservations;
 
 void* client(void* param);
 void* teller(void* param);
+string tellerNames[3] = {"A","B","C"};
 
 int main() {
+
+    for (int i=0; i<3; i++) {
+        cout << tellerNames[i] << endl;
+    }
+
     int numOfClientThreads, numOfTellerThreads;
     pthread_mutex_init(&mutex, NULL);
+    pthread_mutex_init(&reservationMutex, NULL);
     sem_init(&full, 0, 0);
     sem_init(&empty,0,BUFFER_SIZE);
     vector<clientInfo*> clientInfos;
     cout << "Welcome to the Sync-Ticket!" << endl;
 
-    ifstream configFile("configuration_file.txt");
+    ifstream configFile("/mnt/c/Users/berke/Documents/CMPE322/SyncTicket/configuration_file.txt");
     string line, theatreName, seats;
 
     if(!configFile.good()) {
-        cout << "Error" <<endl;
+        cout << "Error reading the file" <<endl;
         return 0;
     }
     else {
@@ -53,7 +60,8 @@ int main() {
         getline(configFile, seats);
         cout << "Theatre Name: " << theatreName << endl;
         cout << "Number of Seats: " << seats << endl;
-        reservations = new bool[stoi(seats)];
+        theatreCapacity = stoi(seats);
+        reservations = new bool[theatreCapacity+1];
 
         while (getline(configFile,line)) {
             stringstream ss(line);
@@ -88,12 +96,14 @@ int main() {
     pthread_t cid[numOfClientThreads];
 
     //Create producer and consumer threads
+    for(int j = 0; j<numOfTellerThreads; j++) {
+        pthread_create(&cid[j], NULL, &teller, &(tellerNames[j]));
+    }
+
     for(int i = 0; i<numOfClientThreads; i++) {
         pthread_create(&pid[i], NULL, &client, clientInfos[i]); //TODO: pass ClientInfo
     }
-    for(int j = 0; j<numOfTellerThreads; j++) {
-        pthread_create(&cid[j], NULL, &teller, clientInfos[j]);
-    }
+
     //Join threads
     for(int i = 0; i < numOfClientThreads; i++) {
         pthread_join(pid[i], NULL);
@@ -103,44 +113,72 @@ int main() {
     }
 
     pthread_mutex_destroy(&mutex);
+    pthread_mutex_destroy(&reservationMutex);
     sem_destroy(&full);
     sem_destroy(&empty);
+    delete [] reservations;
     return 0;
 }
 
 void* client(void* param) {
-    buffer_item item = new clientInfo;
-    for(int i = 0; i < MAX_ITEMS; i++) {
+    buffer_item item = (clientInfo *) param;
+    //cout << item->clientName << endl;
+    usleep(item->arrivalTime * 1000);
+    sem_wait(&empty);
+    //Critical section
+    pthread_mutex_lock(&mutex);
+    //cout << client->clientName << endl;
+    //item->clientName = "CLIENT_" + to_string(START_NUMBER++);
+    buffer[insertPointer] = item;
+    insertPointer = (insertPointer + 1) % BUFFER_SIZE;
+    //printf("Client %u produced %d %s\n", (unsigned int)pthread_self(), item->arrivalTime, item->clientName.c_str());
+    pthread_mutex_unlock(&mutex);
 
-        clientInfo *client = (clientInfo *) param;
-        sleep(2);
-        sem_wait(&empty);
+    sem_post(&full);
+//    }
 
-        //Critical section
-        pthread_mutex_lock(&mutex);
-        item->clientName = "CLIENT_" + to_string(START_NUMBER++);
-        buffer[insertPointer] = item;
-        insertPointer = (insertPointer + 1) % BUFFER_SIZE;
-        printf("Client %u produced %s \n", (unsigned int)pthread_self(), item->clientName.c_str());
-        pthread_mutex_unlock(&mutex);
-        sem_post(&full);
-    }
     pthread_exit(NULL);
 }
 
 void* teller(void* param) {
+    const char* tellerName = (*((string*)param)).c_str();
+    printf("Teller %s has arrived.\n",tellerName);
     buffer_item item;
     for(int i = 0; i < MAX_ITEMS; i++) {
-        sleep(2);
         sem_wait(&full);
 
         //Critical section
         pthread_mutex_lock(&mutex);
         item = buffer[removePointer];
         removePointer = (removePointer + 1) % BUFFER_SIZE;
-        printf("Teller %u consumed %s \n", (unsigned int)pthread_self(), item->clientName.c_str());
+
+        //printf("Teller: %s %u consumed %s\n", tellerName, (unsigned int)pthread_self(), item->clientName.c_str());
         pthread_mutex_unlock(&mutex);
 
+
+        pthread_mutex_lock(&reservationMutex);
+        usleep(item->serviceTime*1000);
+        if (reservations[item->seatNumber]) {
+            // seat full
+            int x;
+            for (x = 1; x < theatreCapacity; x++) {
+                if(!reservations[x]) {
+                    reservations[x] = true;
+                    printf("%s requests seat %d, reserves seat %d. Signed by Teller %s\n",item->clientName.c_str(), item->seatNumber, x, tellerName);
+                    break;
+                }
+            }
+            if (x==theatreCapacity) {
+                printf("%s requests seat %d, reserves seat None. Signed by Teller %s\n", item->clientName.c_str(), item->seatNumber, tellerName);
+            }
+        } else {
+            // seat empty
+            reservations[item->seatNumber] = true;
+            printf("%s requests seat %d, reserves seat %d. Signed by Teller %s\n",item->clientName.c_str(), item->seatNumber, item->seatNumber, tellerName);
+        }
+        pthread_mutex_unlock(&reservationMutex);
+
+
+        sem_post(&empty);
     }
 }
-
